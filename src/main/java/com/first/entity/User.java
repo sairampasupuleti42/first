ea64cbb0b45e36b99c385c7bdde1414e80a8ab66
Sairam <sairampasupuleti.42@gmail.com>
2023-07-09 14:31:02 +0530

    Initial commit

---
+++ src/main/java/com/first/entity/User.java
@@ -0,0 +1,37 @@
+package com.first.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name="LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name="EMAIL", nullable = false)
+    private String email;
+
+    @Column(name="USER_ID", nullable = false)
+    private String userId;
+
+    @Column(name="PASSWORD", nullable = false)
+    private String password;
+
+
+    @Column(name="IS_ACTIVE", nullable = false)
+    private Boolean isActive;
+}
